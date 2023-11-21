5becff192f69982f3aab9458e71f771af3b8f5ad
bogdanluginin <120667209+bogdanluginin@users.noreply.github.com>
2023-11-21 22:34:30 +0200

    Add files via upload

--- myapp/models.py
+++ myapp/models.py
@@ -7,10 +7,25 @@ class Student(models.Model):
     last_name = models.CharField(max_length=100)
     rating = models.FloatField()
 
+    class Meta:
+        db_table = 'students'  # Set the table name
+        verbose_name_plural = 'students'  # Set the plural name
+        ordering = ['last_name', 'first_name']  # Default sorting
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Rating(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='ratings')
     value = models.FloatField()
 
+    class Meta:
+        db_table = 'ratings'  # Set the table name
+        verbose_name_plural = 'ratings'  # Set the plural name
+        ordering = ['-value']  # Default sorting in descending order of value
+
+    def __str__(self):
+        return f"Rating {self.value} for {self.student}"
 
 
 
